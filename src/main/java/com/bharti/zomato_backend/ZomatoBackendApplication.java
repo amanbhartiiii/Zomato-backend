58ac48723a67b79c44d4f5cb010e2dd181a1eca2
amanbhartiiii <amankumarbharti824112@gmail.com>
2025-11-15 14:38:11 +0530

    first commit

---
+++ src/main/java/com/bharti/zomato_backend/ZomatoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bharti.zomato_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bharti/zomato_backend/ZomatoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharti.zomato_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
